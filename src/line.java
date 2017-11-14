61902946d51f77dd749d6aa66300a98c53ae9332
Yongzheng Zhang <yongzheng9405@gmail.com>
2017-11-14 12:51:32 -0500

    Run1

--- src/line.java
+++ src/line.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 /**
  * Created by zhangyongzheng on 11/14/17.
  */
 public class line {
+    String key;
+    ArrayList<String> value;
+
+    line(String key, ArrayList<String> value){
+        this.key = key;
+        this.value = value;
+    }
 }
